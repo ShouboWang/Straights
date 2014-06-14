6a2bece3f64ccb540f6068a803d2f84f261f4e6f
jack <wang.jack94@gmail.com>
2014-06-14 19:11:27 -0400

    Initial Commit

---
+++ Straights/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  Straights
+//
+//  Created by Jack on 2014-06-14.
+//  Copyright (c) 2014 Jack. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
